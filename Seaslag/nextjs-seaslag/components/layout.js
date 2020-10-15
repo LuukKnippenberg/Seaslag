99e16ef4bf65e18d1ffe0ab2c95277c223372416
luuk knippenberg <luuk_knippenberg@hotmail.com>
2020-10-15 14:05:08 +0200

    Home page
    Change the Home page

--- Seaslag/nextjs-seaslag/components/layout.js
+++ Seaslag/nextjs-seaslag/components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Seaslag'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Seaslag'
 
 export default function Layout({ children, home }) {
   return (
@@ -13,45 +13,29 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Go head to head in Battleships with up to 4 players!"
         />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
-        {home ? (
-          <>
-            <img
-              src="/images/seaslag.png"
-              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <img
-                  src="/images/seaslag.png"
-                  className={`${styles.headerImage} ${utilStyles.borderCircle}`}
-                  alt={name}
-                />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
-              </Link>
-            </h2>
-          </>
-        )}
+
+        <div class="navbar">
+          <div class="logo"><h1>Seaslag</h1></div>
+          <div class="menu">
+            <ul>
+              <li class="menu-item">
+                <Link href="/">
+                  <a>Home</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div class="profile">
+                <Link href="/account">
+                  <a>@username</a>
+                </Link>
+          </div>
+        </div>
+        
       </header>
       <main>{children}</main>
       {!home && (
@@ -61,9 +45,9 @@ export default function Layout({ children, home }) {
           </Link>
         </div>
       )}
-      <footer>
-          footertje
-      </footer>
+      {/* <footer>
+          
+      </footer> */}
     </div>
   )
 }
