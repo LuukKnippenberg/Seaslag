064f46b3377679acfd56bec649bc2aed029840ed
Luuk Knippenberg <luuk_knippenberg@hotmail.com>
2020-11-04 17:29:11 +0100

    Dynamic Grid work
    - Grid generation is now based upon the amount of players
    - Added basic grid hover
    - Added some divs and classes for styling and positioning purposes

--- Seaslag/nextjs-seaslag/pages/Game/game.js
+++ Seaslag/nextjs-seaslag/pages/Game/game.js
@@ -2,7 +2,14 @@ import Head from 'next/head'
 import Layout from '../../components/layout'
 import GridComponent from '../../components/grid'
 
+const amountOfPlayers = 4; 
+
+
 export default function Game() {
+
+  const players = ["Jaron"]
+  var playerCounter = 0;
+  
   return (
     <Layout>
       <div className="container">
@@ -11,13 +18,21 @@ export default function Game() {
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <main>
+        <main className={"main main-game"}>
           <h1 className="title">
             Game
           </h1>
-          <div>
-            gridjee
-            <GridComponent />
+          <div className={"gameview"}>
+            {players.map(i => {
+              playerCounter++;
+              return (
+                <section className={"player-screen " + "player-" + playerCounter}>
+                  <h2>{i}</h2>
+                  <GridComponent />
+                </section>
+              );
+              
+            })}
           </div>
         </main>
       </div>
