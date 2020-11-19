2bbf0ab6bfb559733e9d06fb2637feda7eab20d3
Str1XHyper <50529564+Str1XHyper@users.noreply.github.com>
2020-11-19 10:27:27 +0100

    Changes to creategame
     - Made player count an input

--- Seaslag/nextjs-seaslag/pages/Game/creategame.js
+++ Seaslag/nextjs-seaslag/pages/Game/creategame.js
@@ -4,6 +4,7 @@ import styles from '../../styles/Creategame.module.css'
 
 export default function CreateGame() {
 
+
   return (
     <Layout>
       <div className="container">
@@ -16,36 +17,35 @@ export default function CreateGame() {
           <div class="block">
             <form method="POST" id="createGameForm">
               <div class="content">
-                <p className={`text-green ${styles.mt2} ${styles.textCenter}`}>
-                  Amount of People: 4
-              </p>
+                <div>
+                  <label className={`text-green`} name="AmountOfPlayers"> Amount of players: </label>
+                  <input className={`text-green ${styles.inputNumber} ${styles.textCenter}`} type="number" name="PlayerCount" min='2' max='4' defaultValue="2"></input>
+                </div>
                 <div class="container">
                   <div className={styles.row}>
                     <div className={styles.col}>
                       <label>
-                        <input className={styles.mapSelector} type="radio" name="test" value="Siberia" />
+                        <input className={styles.mapSelector} type="radio" name="Map" value="Siberia" defaultChecked />
                         <div className={`text-green black-outline ${styles.imageContainer} ${styles.Siberia} ${styles.textSmall} ${styles.textCenter}`}>Siberia</div>
                       </label>
                     </div>
                     <div className={styles.col}>
                       <label>
-                        <input className={styles.mapSelector} type="radio" name="test" value="Hawaii" />
+                        <input className={styles.mapSelector} type="radio" name="Map" value="Hawaii" />
                         <div className={`text-green black-outline ${styles.imageContainer} ${styles.Hawaii} ${styles.textSmall} ${styles.textCenter}`}>Hawaii</div>
-                        {/* <img src="/images/MapImages/Hawaii Sea.jpg" alt="Hawaii"></img> */}
-                        {/* <p className={styles.TopCenter}>Hawaii</p> */}
                       </label>
                     </div>
                     <div className={styles.col}>
                       <label>
-                        <input className={styles.mapSelector} type="radio" name="test" value="Omaha" />
+                        <input className={styles.mapSelector} type="radio" name="Map" value="Omaha" />
                         <div className={`text-green black-outline ${styles.imageContainer} ${styles.Omaha} ${styles.textSmall} ${styles.textCenter}`}>Omaha Beach</div>
                       </label>
                     </div>
                   </div>
                 </div>
-                <a id="submitForm" className={`text-green ${styles.textCenter} ${styles.HoverBlack}`} onclick="document.getElementById('createGameForm').submit()">
-                  Press Enter to submit
-                </a>
+                <div className={`${styles.center} ${styles.pt3}`}>
+                  <input className={`${styles.submitButton}`} type="submit" value="Press Enter to submit"></input>
+                </div>
               </div>
             </form>
           </div>
